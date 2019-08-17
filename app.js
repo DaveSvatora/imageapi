4deed6bc901ebdc961c9e0d40ca9488fa2b70fe0
Dave Svatora <davesvatora@gmail.com>
2019-08-17 13:44:29 -0500

    no errors, but doesnt work

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({
-  dest: 'uploads/' // this saves your file into a directory called "uploads"
-}); 
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(bodyParser.json());
 
-// It's very crucial that the file name matches the name attribute in your html
-app.post('/', upload.single('file-to-upload'), (req, res) => {
-  res.redirect('/');
+//multer
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './my-uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
 });
+var upload = multer({ storage: storage }).array('pics');
 
-app.listen(3000);
+//cors
+const whitelist = ['http://localhost:8080'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      cb(null, true)
+    } else {
+      cb(new Error('Not allowed by CORS'))
+    }
+  }
+};
+
+//Uploading multiple files
+app.post('/uploadmultiple', cors(corsOptions), function (req, res) {
+  console.log("inpost");
+  upload(req, res, function (err) {
+    // console.log(req.body);
+    //console.log(req.files);
+    if (err) {
+      return res.end("Error uploading file.");
+    }
+    res.end("File is uploaded");
+  });
+})
+
+app.listen(3000, function () {
+  console.log("Working on port 3000");
+});
