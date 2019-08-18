bf76a98ff534dac3d2aaf5c4433587a2b44f1703
Dave Svatora <davesvatora@gmail.com>
2019-08-18 18:59:35 -0500

    working with vue app

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(bodyParser.json());
 // SET STORAGE
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads')
+    cb(null, 'uploads');
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
+    cb(null, file.originalname);
   }
 })
 var upload = multer({ storage: storage })
