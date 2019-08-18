b688a255e9b7789cf30e45fba1a447159de8991e
Dave Svatora <davesvatora@gmail.com>
2019-08-18 13:48:53 -0500

    works

--- app.js
+++ app.js
@@ -7,40 +7,44 @@ const app = express();
 app.use(bodyParser.json());
 
 //multer
+// SET STORAGE
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './my-uploads')
+    cb(null, 'uploads')
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now())
   }
-});
-var upload = multer({ storage: storage }).array('pics');
+})
+var upload = multer({ storage: storage })
 
 //cors
-const whitelist = ['http://localhost:8080'];
+const whitelist = ['http://localhost:8080', 'http://localhost:3000'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      cb(null, true)
+      callback(null, true);
     } else {
-      cb(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
   }
 };
 
+// ROUTES
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
 //Uploading multiple files
-app.post('/uploadmultiple', cors(corsOptions), function (req, res) {
-  console.log("inpost");
-  upload(req, res, function (err) {
-    // console.log(req.body);
-    //console.log(req.files);
-    if (err) {
-      return res.end("Error uploading file.");
-    }
-    res.end("File is uploaded");
-  });
-})
+app.post('/uploadmultiple', cors(corsOptions), upload.array('pics'), (req, res, next) => {
+  const files = req.files
+  if (!files) {
+    const error = new Error('Please choose files')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  res.send(files)
+});
 
 app.listen(3000, function () {
   console.log("Working on port 3000");
